f3d095667b98e44247b9dda3bd060549096ea5fa
RoughTomato <baklazanzaglady@gmail.com>
2021-04-12 20:22:10 +1200

    Added client bit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 extern crate clap;
 use clap::{Arg, App};
+use std::io;
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::str::from_utf8;
 
 fn handle_server(mut stream: TcpStream) {
     let mut data = [0 as u8; 50];
     
     while match stream.read(&mut data) {
-        Ok(size) => {
-            stream.write(&data[0..size]).unwrap();
+        Ok(_) => {
+            stream.write(b"Ok, thank you for sending data!").unwrap();
             true
         },
         Err(_) => {
@@ -42,6 +44,39 @@ fn run_server() {
     drop(listener);
 }
 
+fn client_connect(connection_string: String) {
+    match TcpStream::connect(connection_string) {
+        Ok(mut stream) => {
+            let mut msg = String::new();
+
+            println!("Connected");
+
+            while msg != "q" {
+            
+                io::stdin()
+                .read_line(&mut msg)
+                .expect("Failed to read line");
+                stream.write(msg.as_bytes()).unwrap();
+
+                let mut data = [0 as u8; 50];
+                match stream.read(&mut data) {
+                    Ok(_) => {
+                        let text = from_utf8(&data).unwrap();
+                        println!("{}", text);
+                    },
+                    Err(e) => {
+                        println!("Error! failed to receive data: {}", e);
+                    }
+                }
+            }
+        },
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
+    println!("Connection terminated");
+}
+
 fn main() {
     let matches = App::new("chatCrab")
     .version("v0.0.1")
@@ -61,10 +96,14 @@ fn main() {
     //TODO: consider playing with Rusts GUI libs and allow for CLI or GUI selection
 
     let server = matches.is_present("server");
+    let client = matches.is_present("client");
 
-    if server {
+    if server && !client {
         println!("Running chatCrab in server mode");
         run_server();
+    } else if client && !server {
+        let client_data = matches.value_of("client").unwrap_or("127.0.0.1:3333");
+        client_connect(client_data.to_string());
     }
 
 }
